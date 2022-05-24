5adb730c7a05b78a15110419f24546da8ec93351
I2rys <i2rys@protonmail.com>
2022-05-24 10:25:00 +0800

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Dependencies
+const request = require("request-async")
+
+// Main
+async function check(ip){
+    var response = await request(`http://167.172.85.80/api/check/proxy?ip=${ip}`)
+    response = JSON.parse(response.body)
+
+    return response.proxy
+}
+
+module.exports = {
+    check
+}
